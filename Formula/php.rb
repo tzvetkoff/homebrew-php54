4c5c85c4ef2fc46b8485f655bf3d185a555be08e
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2012-06-19 13:48:29 +0300

    updated to php 5.4.4

--- Formula/php.rb
+++ Formula/php.rb
@@ -10,9 +10,9 @@ end
 
 class Php < Formula
   homepage 'http://php.net'
-  url 'http://www.php.net/get/php-5.4.3.tar.bz2/from/this/mirror'
-  md5 '51f9488bf8682399b802c48656315cac'
-  version '5.4.3'
+  url 'http://www.php.net/get/php-5.4.4.tar.bz2/from/this/mirror'
+  md5 '1fd98dc3f6f3805cd67bff12a26ed77f'
+  version '5.4.4'
 
   head 'https://svn.php.net/repository/php/php-src/branches/PHP_5_4', :using => :svn
 
